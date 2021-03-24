82823a9f794950345310f07716f9ad24a9022e9a
Manish Verma <vermam318@gmail.com>
2021-03-24 14:46:26 +0530

    Gesture detector added in GridTile

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,3 +1,4 @@
+import 'package:ShopApp/screens/product_detail_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
@@ -8,18 +9,27 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(10.0,),
-          child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+      borderRadius: BorderRadius.circular(
+        10.0,
+      ),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           leading: IconButton(
             icon: Icon(Icons.favorite),
             onPressed: () {},
             color: Theme.of(context).accentColor,
-
           ),
           backgroundColor: Colors.black87,
           title: Text(
